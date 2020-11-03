dc700857f374d166a214fc1decfc42512ad86aa0
leesujin <lsj990611@gmail.com>
2020-11-04 00:29:22 +0900

    Merge branch 'master' of https://github.com/Smineee11/HomeTraise
     Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/hometraise/MainActivity.java
+++ app/src/main/java/com/example/hometraise/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.hometraise;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    //인텐트 처리
+    public  void displaygrid(View v) {
+        int id = v.getId();
+        LinearLayout layout = (LinearLayout)v.findViewById(id);
+        String tag = (String) layout.getTag();
+
+        Toast.makeText(this, tag, Toast.LENGTH_LONG);
+        Intent it = new Intent(this, grid.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
+    }
+
 }
