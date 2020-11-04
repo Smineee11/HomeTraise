480a485eb6e99e788a4e35354d15da1d257baba9
Smineee11 <smin3407ch@gmail.com>
2020-11-04 13:38:29 +0900

    make basic layout (characterdetail)

--- app/src/main/java/com/example/hometraise/MainActivity.java
+++ app/src/main/java/com/example/hometraise/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hometraise;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void clickShop(View view){
+        Toast.makeText(this,"Shop버튼(테스트용)",Toast.LENGTH_SHORT).show();
+    }
+    public void clickExercise(View view){
+        Toast.makeText(this,"Exercise버튼(테스트용)",Toast.LENGTH_SHORT).show();
     }
 }
