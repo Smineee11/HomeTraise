ad9fe54da7d6cb6fd810825936c631405b5d28a8
compuTasha <mijookim.tasha@gmail.com>
2020-11-18 11:48:35 +0900

    modify first page(add logo and buttons) : MainActivity.java

--- app/src/main/java/com/example/hometraise/MainActivity.java
+++ app/src/main/java/com/example/hometraise/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hometraise;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -10,21 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton, makenewButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.start);
+        makenewButton = (Button)findViewById(R.id.makenew);
     }
 
     //인텐트 처리
     public  void displaygrid(View v) {
-        int id = v.getId();
-        LinearLayout layout = (LinearLayout)v.findViewById(id);
-        String tag = (String) layout.getTag();
-
-        Toast.makeText(this, tag, Toast.LENGTH_LONG).show();
         Intent it = new Intent(this, character_detail.class);
-        it.putExtra("it_tag", tag);
         startActivity(it);
     }
 
