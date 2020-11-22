8dbdf9a1bf617a1cf031a92c24cec72408b114a4
compuTasha <mijookim.tasha@gmail.com>
2020-11-22 13:07:18 +0900

    Modify layout(closet, purchase) : closet_detail.xml, purchase.xml

--- app/src/main/java/com/example/hometraise/Closet.java
+++ app/src/main/java/com/example/hometraise/Closet.java
@@ -8,10 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Closet extends AppCompatActivity {
 
+    Button back, change;
+    ImageView picture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store_detail);
+
+        back = (Button)findViewById(R.id.closet_back);
+        change = (Button)findViewById(R.id.closet_change);
+        picture = (ImageView)findViewById(R.id.closet_image);
 
     }
 
