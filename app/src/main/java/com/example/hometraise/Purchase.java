32a4f07e7f1a15cd9badf3dbf9686f7c0faf1094
leesujin <lsj990611@gmail.com>
2020-12-15 12:17:46 +0900

    store와 closet 연결

--- app/src/main/java/com/example/hometraise/Purchase.java
+++ app/src/main/java/com/example/hometraise/Purchase.java
@@ -31,7 +31,8 @@ public class Purchase extends AppCompatActivity {
     private Context context;
     String id;
     int point;
-
+    String selectedName;
+    int selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,8 +46,8 @@ public class Purchase extends AppCompatActivity {
         id = intent.getExtras().getString("id");
         point = intent.getExtras().getInt("point");
         if(intent.getExtras()!=null){
-            String selectedName  = intent.getStringExtra("name");
-            int selectedImage = intent.getIntExtra("image", 0);
+            selectedName  = intent.getStringExtra("name");
+            selectedImage = intent.getIntExtra("image", 0);
             picture.setImageResource(selectedImage);
         }
 
@@ -56,61 +57,132 @@ public class Purchase extends AppCompatActivity {
 
         //Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show();
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        //if 구매 가능한 경우
+        if (point >=500) {
 
-        alertDialogBuilder.setTitle("구매 완료");
-        alertDialogBuilder.setMessage("구매가 완료되었습니다. ");
-        alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Characters").child(id);
-                dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        CharacterData data = snapshot.getValue(CharacterData.class);
-                        Log.d("NAME ", data.name);
-                        Log.d("POINT ", String.valueOf(data.point));
+            alertDialogBuilder.setTitle("구매 완료");
+            alertDialogBuilder.setMessage("구매가 완료되었습니다. ");
+            alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Closet").child(id);
+                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            ClosetData data = snapshot.getValue(ClosetData.class);
+                            if (data == null)
+                                System.out.println("Undefined user");
+                            else {
+                                if (selectedName.equals("halloween")) {
+                                    if (data.halloween == 0 ) {
+                                        //구매 가능
+                                        final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Characters").child(id);
+                                        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                CharacterData data = snapshot.getValue(CharacterData.class);
+                                                Log.d("NAME ", data.name);
+                                                Log.d("POINT ", String.valueOf(data.point));
 
-                        if (data == null)
-                            System.out.println("Undefined user");
+                                                if (data == null)
+                                                    System.out.println("Undefined user");
 
-                        else {
-                            data.point = data.point - 500;
-                            Log.d("CHANGE ", String.valueOf(data.point));
+                                                else {
+
+                                                    data.point = data.point - 500;
+                                                    Log.d("CHANGE ", String.valueOf(data.point));
+
+                                                }
+
+                                                String key = dbRef.child("Characters").push().getKey();
+                                                CharacterData newdata = new CharacterData(data.name, data.point, data.clothes);
+                                                Map<String, Object> newdataValues = newdata.toMap();
+
+                                                dbRef.setValue(newdataValues);
+
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+                                                Log.e("The read failed: ", error.getMessage());
+                                            }
+                                        });
+                                        data.halloween = 1;
+                                    }
+                                } else if (selectedName.equals("christmas")) {
+                                    if (data.christmas == 0) {
+                                        //구매 가능
+                                        final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Characters").child(id);
+                                        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                CharacterData data = snapshot.getValue(CharacterData.class);
+                                                Log.d("NAME ", data.name);
+                                                Log.d("POINT ", String.valueOf(data.point));
+
+                                                if (data == null)
+                                                    System.out.println("Undefined user");
+
+                                                else {
+
+                                                    data.point = data.point - 500;
+                                                    Log.d("CHANGE ", String.valueOf(data.point));
+
+                                                }
+
+                                                String key = dbRef.child("Characters").push().getKey();
+                                                CharacterData newdata = new CharacterData(data.name, data.point, data.clothes);
+                                                Map<String, Object> newdataValues = newdata.toMap();
+
+                                                dbRef.setValue(newdataValues);
+
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+                                                Log.e("The read failed: ", error.getMessage());
+                                            }
+                                        });
+                                        data.christmas = 1;
+
+                                    }
+                                }
+                                String key = dbRef.child("Closet").push().getKey();
+                                ClosetData newdata = new ClosetData(data.exercise, data.halloween, data.christmas);
+                                Map<String, Object> newdataValues = newdata.toMap();
+
+                                dbRef.setValue(newdataValues);
+
+                            }
                         }
 
-                        String key = dbRef.child("Characters").push().getKey();
-                        CharacterData newdata = new CharacterData(data.name, data.point, data.clothes);
-                        Map<String, Object> newdataValues = newdata.toMap();
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
 
-                        dbRef.setValue(newdataValues);
+                        }
+                    });
 
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.e("The read failed: ", error.getMessage());
-                    }
-                });
-                finish();
-            }
+                    finish();
+                }
 
-        });
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+        else {
+            //구매 불가능한 경우
+            alertDialogBuilder.setTitle("Warning");
+            alertDialogBuilder.setMessage("구매하실 수 없습니다.. ");
+            alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
 
-        //else 구매 불가능한 경우
-//        alertDialogBuilder.setTitle("Warning");
-//        alertDialogBuilder.setMessage("구매하실 수 없습니다.. ");
-//        alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                finish();
-//            }
-//
-//        });
-//        AlertDialog alertDialog = alertDialogBuilder.create();
-//        alertDialog.show();
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
 
     public void cancelClicked(View v){
